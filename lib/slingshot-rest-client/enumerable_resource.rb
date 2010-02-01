d628c008aa0887bdeb155b4e224524402f80e66e
Jay Stotz <jason.stotz@gmail.com>
2010-02-01 17:47:05 -0500

    Added experimental feature for iterating through paginated collection resources using #each.
    For performance reasons, #each yields representations, not resources.
    
    To use:  require 'slingshot-rest-client/enumerable_resource'
    
    Note: only implements #each so not yet a real Ruby Enumerable.

---
+++ lib/slingshot-rest-client/enumerable_resource.rb
@@ -0,0 +1,43 @@
+##
+# Experimental:
+# If included, allows iterating over collection resources as though they were enumerables
+#
+# @example
+# resource = SlingshotRestClient::Resource.new(
+#   'http://services.slingshot.local/customers',
+#   :user => 'admin',
+#   :password => 'admin'
+# )
+#   
+# resource.each do |customer|
+#   puts customer.name
+# end
+#
+module SlingshotRestClient
+  module EnumerableResource
+    def each_page
+      total_pages = nil
+      page = 1
+      while total_pages.nil? or page <= total_pages
+        page_resource = self.class.new(self.url + "?page=#{page}", options)
+        puts page_resource.url
+        data = page_resource.get
+        yield data
+        total_pages ||= data[:pages]
+        page += 1
+      end
+    end
+
+    def each
+      each_page do |page|
+        page[:results].each do |result|
+          yield result
+        end
+      end
+    end
+  end
+end
+
+class SlingshotRestClient::Resource
+  include SlingshotRestClient::EnumerableResource
+end
