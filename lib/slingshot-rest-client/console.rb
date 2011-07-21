c0f1cd39e382ec59f3dab805d27f4b74abb1b62f
Jay Stotz <jason.stotz@gmail.com>
2011-07-21 11:34:53 -0400

    Added a simple testing console script

---
+++ lib/slingshot-rest-client/console.rb
@@ -0,0 +1,10 @@
+CLIENT = SlingshotRestClient::Resource.new \
+  'http://sagamore.local:8001',
+  :user => 'admin',
+  :password => 'admin'
+  
+def client
+  CLIENT
+end
+  
+puts "'client' method available for testing: #{client.inspect}"
