3e5acf6df9ce99a0d3bf22cb506d8c153d9340f2
Jay Stotz <jason.stotz@gmail.com>
2018-02-21 17:14:45 -0500

    Allow running focused specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,11 @@ require 'springboard-retail'
 require 'webmock/rspec'
 require 'shared_client_context'
 
+RSpec.configure do |c|
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
+end
+
 class String
   def to_uri
     Addressable::URI.parse(self)
